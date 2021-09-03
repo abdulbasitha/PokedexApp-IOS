33458cb2e10eaca2e16e22697de8af3f39fe0f4f
abdulbasitha <basith.ambalavan@gmail.com>
2021-09-03 22:06:43 +0530

    initail commit

---
+++ PokedexApp/PokedexAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexAppApp.swift
+//  PokedexApp
+//
+//  Created by ABDUL BASITH A on 03/09/21.
+//
+
+import SwiftUI
+
+@main
+struct PokedexAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
